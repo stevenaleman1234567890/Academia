588c26691291bdebbcb46b0a8c4c8ba7b535dd91
stevenaleman126626 <brendaraudez25@gmail.com>
2025-07-02 08:53:31 -0600

    Agrege las clases persona y profesor

---
+++ OneDrive/Documentos/NetBeansProjects/Academia1/src/Clases/Persona.java
@@ -0,0 +1,54 @@
+package Clases;
+
+public class Persona {
+    private String nombres;
+    private String apellidos;
+    private String email;
+    private String telefono;
+
+    public Persona(String nombres, String apellidos, String email, String telefono) {
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.email = email;
+        this.telefono = telefono;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "nombres=" + nombres + ", apellidos=" + apellidos + ", email=" + email + ", telefono=" + telefono + '}';
+    }
+    
+    
+}
---
+++ OneDrive/Documentos/NetBeansProjects/Academia1/src/Clases/Profesor.java
@@ -0,0 +1,29 @@
+package Clases;
+
+public class Profesor extends Persona {
+    private String idProfesor;
+    private String especialidad;
+    private String tipoContrato;
+
+    public Profesor(String nombres, String apellidos, String email, String telefono,
+                    String idProfesor, String especialidad, String tipoContrato) {
+        super(nombres, apellidos, email, telefono);
+        this.idProfesor = idProfesor;
+        this.especialidad = especialidad;
+        this.tipoContrato = tipoContrato;
+    }
+
+    public String getIdProfesor() { return idProfesor; }
+    public String getEspecialidad() { return especialidad; }
+    public String getTipoContrato() { return tipoContrato; }
+
+    public void setIdProfesor(String idProfesor) { this.idProfesor = idProfesor; }
+    public void setEspecialidad(String especialidad) { this.especialidad = especialidad; }
+    public void setTipoContrato(String tipoContrato) { this.tipoContrato = tipoContrato; }
+
+    @Override
+    public String toString() {
+        return super.toString() + " | ID: " + idProfesor + ", Especialidad: " + especialidad + ", Contrato: " + tipoContrato;
+    }
+}
+
